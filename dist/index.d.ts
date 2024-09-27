5e5b2d2c7ade09f9ba8547bb4521791ded32a525
Rashidprojects <166205136+Rashidprojects@users.noreply.github.com>
2024-09-27 11:06:56 +0530

    Delete dist/index.d.ts

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-// dist/index.d.ts
-declare module 'ui-design-system' {
-    export interface ButtonProps {
-        label: string;
-        onClick: () => void;
-    }
-    
-    export declare const Button: (props: ButtonProps) => JSX.Element;
-}
